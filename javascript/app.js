72928fad9650d74bdc741c3e3a60cceaa87b78b1
unknown <chizaanicet@gmail.com>
2023-12-06 01:11:15 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,23 @@
-const scroll = document.querySelector('.top-icon');
-scroll.addEventListener('click', () =>{
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-    })
-})
+jQuery(function($){
+    const $header = $('.header');
+    $(window).scroll(function(event){
+        const $current = $(this).scrollTop();
+        if($current > 560){
+            $header.addClass('header-color');
+        }
+        else{
+            $header.removeClass('header-color');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scroll = document.querySelector('.top-icon');
+    scroll.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        });
+    });
+});
